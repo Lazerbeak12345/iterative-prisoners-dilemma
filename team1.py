b0ce2f9e1b89d5188ed0027195ba02bf1592c40e
weirdprogramer <rub122000@gmail.com>
2016-11-22 14:26:39 -0700

    Update team1.py

--- team1.py
+++ team1.py
@@ -8,14 +8,17 @@
 
 team_name = 'Tacos' # Only 10 chars displayed.
 strategy_name = 'Tortilla wrap'
-strategy_description = 'alwalys collude'# will look into this later
+strategy_description = 'Collude, then alternate.'# will look into this later
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
+    ''' make my move based on the history of the player 
     
-    Make my move.
-    Returns 'c' or 'b'. 
+ +    history: a string with one letter (c or b) per round that has been played with this opponent.
+ +    their_history: a string of the same length as history, possibly empty. 
+ +    The first round between these two players is my_history[0] and their_history[0]
+ +    The most recent round is my_history[-1] and their_history[-1]
+ +    
+ +    Returns 'c' or 'b' for collude or betray. 
     '''
     OPTIONS=['c','b']
     undecided=false#set to true if don't know what to do
@@ -27,10 +30,10 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    if undecided:
-        return random.choice(['c','b'])
-    return 'c'
-
+      if len(my_history)%2 == 0:
+ +        return 'c'
+ +    else:
+ +        return 'b'
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
